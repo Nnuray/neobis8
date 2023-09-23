d2c4ae3664156aa5fd4f389d43726ff920eccdfe
Nuray <nuray.kavkaeva.04@gmail.com>
2023-09-23 09:45:58 +0600

    Commit

---
+++ src/main/java/com/example/neobis8/Neobis8Application.java
@@ -0,0 +1,13 @@
+package com.example.neobis8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neobis8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neobis8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neobis8/Neobis8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neobis8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neobis8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
