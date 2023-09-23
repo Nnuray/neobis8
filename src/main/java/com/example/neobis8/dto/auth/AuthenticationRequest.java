8c7506b35283b34aff125a5d50ddeb01ff2cc62c
Nuray <nuray.kavkaeva.04@gmail.com>
2023-09-23 11:23:03 +0600

    auth

---
+++ src/main/java/com/example/neobis8/dto/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.example.neobis8.dto.auth;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String login;
+    @JsonIgnore
+    private String password;
+}
--- src/main/java/com/example/neobis8/entity/User.java
+++ src/main/java/com/example/neobis8/entity/User.java
@@ -1,4 +1,24 @@
 package com.example.neobis8.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String profilePicturePath;
+    private String login;
+    private String fio;
+
 }
