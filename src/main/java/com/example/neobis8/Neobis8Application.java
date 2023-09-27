625163290ccadcd3389243132a4fa22e5aabaee8
Nuray <nuray.kavkaeva.04@gmail.com>
2023-09-28 00:36:52 +0600

    commit

--- src/main/java/com/example/neobis8/Neobis8Application.java
+++ src/main/java/com/example/neobis8/Neobis8Application.java
@@ -8,6 +8,8 @@ public class Neobis8Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Neobis8Application.class, args);
+        System.out.println("Hello");
     }
+
 
 }
